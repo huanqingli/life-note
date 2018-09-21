11cc9ef6a418e7bf3200af8711c329b80b3c5774
huanqingli <78503390@qq.com>
2018-09-21 10:22:42 +0800

    flask

--- 后端/python/Flask/tutorial/flaskr/__init__.py
+++ 后端/python/Flask/tutorial/flaskr/__init__.py
@@ -18,12 +18,15 @@ def create_app(test_config = None):
     except OSError:
         pass
 
+    from . import db
+    db.init_app(app)
+
+    from . import bp
+    app.register_blueprint(auth.bp)
+
     @app.route('/hello')
     def hello():
         return 'Hello World'
-
-    from . import db
-    db.init_app(app)
     
     return app
 
