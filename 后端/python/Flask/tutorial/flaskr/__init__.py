bbb65d3d8f68b1fed06da269019377d4388c2f21
huanqingli <78503390@qq.com>
2018-09-26 09:27:20 +0800

    job

--- 后端/python/Flask/tutorial/flaskr/__init__.py
+++ 后端/python/Flask/tutorial/flaskr/__init__.py
@@ -21,7 +21,7 @@ def create_app(test_config = None):
     from . import db
     db.init_app(app)
 
-    from . import bp
+    from . import auth
     app.register_blueprint(auth.bp)
 
     @app.route('/hello')
